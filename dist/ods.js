dda29b222461abe2b7612b7401c0ac8dc2fcbdf7
Ravi Bheda <bheda91@gmail.com>
2018-01-12 16:16:35 +0200

    updated ods.js

--- dist/ods.js
+++ dist/ods.js
@@ -7,9 +7,11 @@ var ODS = {};
 var get_utils = function() {
 	if(typeof XLSX !== 'undefined') return XLSX.utils;
 	if(typeof module !== "undefined" && typeof require !== 'undefined') try {
-		return require('../' + 'xlsx').utils;
+		console.log("-=========--======in ifffff=====-==================")
+		return require('./' + 'xlsx').utils;
 	} catch(e) {
-		try { return require('./' + 'xlsx').utils; }
+		console.log("-=========--======in  catch=====-==================")
+		try { return require('../' + 'xlsx').utils; }
 		catch(ee) { return require('xl' + 'sx').utils; }
 	}
 	throw new Error("Cannot find XLSX utils");
